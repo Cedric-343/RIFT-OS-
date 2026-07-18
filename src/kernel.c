e1d01c459665007075223a2f426c9e05d788e844
MacOS Distro <cedriku343@gmail.com>
2026-07-18 19:53:12 +0100

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -7,5 +7,20 @@ void boot_message(void) {
         VGA_MEM[i * 2 + 1] = 0x07;
     }
 }
+void main_kernel_function(void) {
 
 
+
+}
+
+void sys_exit(void) {
+    
+
+
+}
+
+void* syscall_table[] = {
+    [0] = sys_exit
+    [1] = sys_write
+    [2] = sys_read
+}
